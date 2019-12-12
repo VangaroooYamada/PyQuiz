5db58dcdc851ca8fa65d74b5adf60f4176b0e0e9
Vangarooo Yamada <vangarooo.yamada@gmail.com>
2019-12-12 18:24:53 +0900

    Clean code
    Refactor (make some methods)

--- main.py
+++ main.py
@@ -10,92 +10,114 @@ import pyocr.builders
 from PIL import Image
 
 
+# OCR Setting ***************
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
     print("No OCR tool found")
     sys.exit(1)
-
-tool = tools[0]
+tool = tools[0]     # Setting OCR tool
 print("Will use tool '%s'" % (tool.get_name()))
-
 
 langs = tool.get_available_languages()
 print("Available languages: %s" % ", ".join(langs))
-lang = langs[1]
-print("Will use lang '%s'" % (lang))
+lang = langs[1]     # Setting Language for OCR (default 'jpn')
+print("Will use lang '%s'\n" % (lang))
 
 
-inames = glob.iglob('./imgs/*.png')
-
-with open('./trans_dict.yaml', encoding='utf-8') as yf:
-    trans_dict = yaml.safe_load(yf)
+# Data for Excel (Quiz List) ***************
+inames = glob.iglob('./imgs/*.png')     # Image Path Generator
 QLPath = './QuizList.xlsx'
 quiz_list = px.load_workbook(QLPath)
-ws = quiz_list.worksheets[-1]
-MR = quiz_list.worksheets[0].max_row
-BR = 2          # Brank Row's num
-i_count = 0     # Processed Image Counter
-q_count = 0     # Added Quiz Counter
+with open('./trans_dict.yaml', encoding='utf-8') as yf:
+    trans_dict = yaml.safe_load(yf)     # Dictionary for Translation txt
 
 
-while BR < MR+1:
-    if not ws['B' + str(BR)].value is None:
-        BR += 1
-        continue
-    break
+def time_counter(func):     # Decolator to count processing time
+    def count_func(*args, **kwargs):
+        start = time.time()
+        i_count, q_count = func(*args, **kwargs)
+        end = time.time() - start
+        return i_count, q_count, end
+    return count_func
 
 
-start = time.time()
+def excel_safesaver(func):      # Decolator to check Excel's status
+    def processing(*args, **kwargs):
+        i_count, q_count = func(*args, **kwargs)
+        for i in range(3):
+            try:
+                open('./QuizList.xlsx', 'r+')
+            except IOError:
+                print('*****************************************')
+                print('Excel file is opened! Please close Excel.')
+                print('({} times left)'.format(3 - i))
+                print('*****************************************')
+                _ = input('Close Excel and press any key.')
+            else:
+                break
+        else:
+            print('Processing Error!')
+            quit(1)
 
-for img in inames:
-    txt = tool.image_to_string(
-        Image.open(img),
-        lang='jpn',
-        builder=pyocr.builders.TextBuilder(tesseract_layout=3)
-    )
-
-    txt = txt.translate(str.maketrans(trans_dict)).replace('\n', '')
-    for q in re.finditer(r'(問題正解率:\d+\%|Q\.)(.*?でしょう？)', txt):
-        # print(q.groups()[1])
-        if BR > MR:
-            tmp_num = int(ws.title[:-1]) + 1000
-            quiz_list.copy_worksheet(quiz_list.worksheets[0])
-            ws = quiz_list.worksheets[-1]
-            ws.title = str(tmp_num) + '-'
-            BR = 2
-
-        ws['B' + str(BR)].value = q.groups()[1]
-        q_count += 1
-        BR += 1
-
-    i_count += 1
-
-end_time = time.time() - start
+        quiz_list.save('./QuizList.xlsx')
+        return i_count, q_count
+    return processing
 
 
-for i in range(3):
-    try:
-        myfile = open('./QuizList.xlsx', 'r+')
-    except IOError:
-        print('*****************************************')
-        print('Excel file is opened! Please close Excel.')
-        print('({} times left)'.format(3 - i))
-        print('*****************************************')
-        key = input('Close Excel and press any key.')
-    else:
+@time_counter
+@excel_safesaver
+def image_processor(img_gen):   # method to write processed text to Excel
+    ws = quiz_list.worksheets[-1]
+    MR = quiz_list.worksheets[0].max_row
+    BR = 2          # Brank Row's num
+    i_count = 0     # Processed Image Counter
+    q_count = 0     # Added Quiz Counter
+
+    while BR < MR+1:
+        if not ws['B' + str(BR)].value is None:
+            BR += 1
+            continue
         break
-else:
-    print('Processing Error!')
-    quit(1)
 
-quiz_list.save('./QuizList.xlsx')
+    for img in inames:
+        txt = tool.image_to_string(
+            Image.open(img),
+            lang='jpn',
+            builder=pyocr.builders.TextBuilder(tesseract_layout=3)
+        )
+
+        txt = txt.translate(str.maketrans(trans_dict)).replace('\n', '')
+        for q in re.finditer(r'(問題正解率:\d+\%|Q\.)(.*?でしょう？)', txt):
+            # print(q.groups()[1])
+            if BR > MR:
+                tmp_num = int(ws.title[:-1]) + 1000
+                quiz_list.copy_worksheet(quiz_list.worksheets[0])
+                ws = quiz_list.worksheets[-1]
+                ws.title = str(tmp_num) + '-'
+                BR = 2
+
+            ws['B' + str(BR)].value = q.groups()[1]
+            q_count += 1
+            BR += 1
+
+        i_count += 1
+
+    return i_count, q_count
 
 
-print('''
+if __name__ == '__main__':
+
+    i_count, q_count, end_time = image_processor(inames)
+
+    if not i_count:
+        print('No Image is processed.')
+        print('Please put any image to "img" directory.')
+    else:
+        print('''
 **********************
 SUCCESS
 {} Photos Processed
 {} Quiz Added
 Process Time: {}s
 **********************
-'''.format(i_count, q_count, round(end_time, 3)))
+        '''.format(i_count, q_count, round(end_time, 3)))
